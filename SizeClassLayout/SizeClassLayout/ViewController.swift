b70579b03b2c9df7d5a5d55dac1888e8221f68f1
VarunChillara <varun27896@gmail.com>
2019-11-12 19:07:15 -0600

    Add files via upload
    Dynamic Auto Layout

---
+++ SizeClassLayout/SizeClassLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClassLayout
+//
+//  Created by Varun Chillara on 11/12/19.
+//  Copyright © 2019 Varun Chillara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
